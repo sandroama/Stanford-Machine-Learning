1ba63306d33b3536e780ccbc7abdcf30e2303654
SandroAma <sandroama123@gmail.com>
2022-07-22 15:03:44 -0400

    mini update

--- Week-3/advanced_optimization.m
+++ Week-3/advanced_optimization.m
@@ -1,3 +1,6 @@
 options = optimset('GradObj','on','MaxIter','100');
 initialTheta = zeros(2,1)
 [optTheta, functionval, exitFlag] = fminunc(@costFunction, initialTheta, options)
+
+
+help fminunc // Documentation, function minimum unconstrained
