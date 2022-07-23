cd0c9f2ab9fc2c0fbe9593382aca9acb16b440a1
SandroAma <sandroama123@gmail.com>
2022-07-22 21:10:46 -0400

    Completed Week 3

--- Week-3/ex2/costFunctionReg.m
+++ Week-3/ex2/costFunctionReg.m
@@ -11,12 +11,22 @@ m = length(y); % number of training examples
 J = 0;
 grad = zeros(size(theta));
 
-% ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
 %               You should set J to the cost.
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+pred = sigmoid(X * theta);
+leftF = -y' * log(pred);
+rightF = (1-y') * log(1-pred);
+
+thetaZero = theta;
+thetaZero(1)=0;
+
+lambdaCostPart = (lambda/(2*m)) * sum(thetaZero .^ 2);
+lambdaGradPart = lambda/m * thetaZero;
+J=(1/m) * (leftF - rightF) + lambdaCostPart;
+grad = ( (1/m) * (X'*(pred-y) ) ) + lambdaGradPart;
 
 
 
